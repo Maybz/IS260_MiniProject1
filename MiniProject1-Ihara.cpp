81c92bd3bd938a495a5cd29238adbb8b35c821eb
Maybz <maybihar@comfsm.fm>
2015-09-28 12:05:05 +1100

    Update MiniProject1-Ihara.cpp

--- MiniProject1-Ihara.cpp
+++ MiniProject1-Ihara.cpp
@@ -7,8 +7,13 @@
   int main()
 {
     std::int number;
+    std::int forecast;
     std::cout << "Please input either 0.1 or 0.2 : " << endl;
     std::cin >> number;
+      if (number == 0.1);
+      {
+        cout << "Your prediction is: " cout << forecast;
+      }
     std::cout << endl;
 }
 
