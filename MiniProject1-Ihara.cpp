273850bb07372e2074bd3b008cacb0f41d6fb4fd
Maybz <maybihar@comfsm.fm>
2015-09-28 17:10:15 +1100

    Update MiniProject1-Ihara.cpp

--- MiniProject1-Ihara.cpp
+++ MiniProject1-Ihara.cpp
@@ -2,20 +2,36 @@
 // Submitted to: Edper Castro
 // Mini Project 1
 #include <iostream>
-#include <string>
+using namespace std;
   
   int main()
 {
-    std::int number;
-    std::int forecast;
-    std::cout << "Please input either 0.1 or 0.2 : " << endl;
-    std::cin >> number;
-      if (number == 0.1);
-      {
-        cout << "Your prediction is: " cout << forecast;
-        forecast = number + 1 ()
-      }
-    std::cout << endl;
+    int month;
+    double demand;
+    double forecast;
+    double forecast_2;
+    double prev_forecast;
+    double parameter_1 = 0.1;
+    double parameter_2 = 0.2;
+    
+    cout << "Enter actual demand for month 1: ";
+    cin >> demand;
+    cout << endl;
+    
+    while ( month < 12)
+    {
+        cout << "This is your prediction with 0.1: " << forecast << endl;
+            forecast = parameter_1 * demand + ( 1 - parameter_1 ) *  demand;
+            
+        cout << "This your prediction with 0.2: " << forecast << endl;
+            prev_forecast = forecast;
+            forecast_2 = parameter_2 * demand + ( 1 - parameter_2 ) * prev_forecast;
+    }
+    
+    
+    cout << endl;
+    cout << "That is all I can Predict! " << endl;
+    return 0;
 }
 
 
