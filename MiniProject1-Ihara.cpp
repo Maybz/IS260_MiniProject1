1eddb2da8141ae248b75cb3f7f71bd14b3b30111
Maybz <maybihar@comfsm.fm>
2015-09-30 17:17:45 +1100

    Update MiniProject1-Ihara.cpp

--- MiniProject1-Ihara.cpp
+++ MiniProject1-Ihara.cpp
@@ -6,29 +6,49 @@ using namespace std;
   
   int main()
 {
-    int month;
-    double demand;
+    int mnthcount = 1;
+    double actual_demand;
+    double prev_act_demand;
     double forecast;
     double forecast_2;
     double prev_forecast;
     double parameter_1 = 0.1;
     double parameter_2 = 0.2;
     
-    cout << "Enter actual demand for month 1: ";
-    cin >> demand;
-    cout << endl;
-    
-    while ( month < 12)
+    while (mnthcount <= 12)
     {
+       cout << "Enter actual demand for month " << mnthcount++ << ": ";
+       cin >> actual_demand;
+       cout << endl;
+       
         cout << "This is your prediction with 0.1: " << forecast << endl;
-            forecast = parameter_1 * demand + ( 1 - parameter_1 ) *  demand;
-            
+            forecast = parameter_1 * actual_demand + ( 1 - parameter_1 ) *  actual_demand;
+            cout.setf(ios::fixed);
+		        cout.setf(ios::showpoint);
+		        cout.precision(2);
         cout << "This your prediction with 0.2: " << forecast << endl;
             prev_forecast = forecast;
-            forecast_2 = parameter_2 * demand + ( 1 - parameter_2 ) * prev_forecast;
+            forecast_2 = parameter_2 * actual_demand + ( 1 - parameter_2 ) * prev_forecast;
+            cout.setf(ios::fixed);
+		        cout.setf(ios::showpoint);
+		        cout.precision(2);
+		      cout << endl;
     }
-    
-    
+    if (mnthcount = 1)
+    {
+      forecast = actual_demand; 
+      21forecast_2 = actual_demand;
+    }
+    else 
+    {
+      prev_act_demand = actual_demand;
+      prev_forecast = forecast;
+      forecast = parameter_1 * prev_act_demand + ( 1 - parameter_1 ) * prev_forecast;
+      
+      prev_act_demand = actual_demand;
+      prev_forecast = forecast_2;
+      forecast_2 = parameter_2 * prev_act_demand + ( 1 - parameter_2 ) * prev_forecast;
+    }
     cout << endl;
     cout << "That is all I can Predict! " << endl;
     return 0;
