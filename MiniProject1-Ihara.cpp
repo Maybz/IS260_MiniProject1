e8d22556205b0dc8e15c95e7a703fb9744c226fb
Maybz <maybihar@comfsm.fm>
2015-10-02 20:52:50 +1000

    Update MiniProject1-Ihara.cpp

--- MiniProject1-Ihara.cpp
+++ MiniProject1-Ihara.cpp
@@ -6,49 +6,56 @@ using namespace std;
   
   int main()
 {
-    int mnthcount = 1;
+    int mnthcount = 0;
     double actual_demand;
+    double total_demand;
+    double total_demand_2;
     double prev_act_demand;
-    double forecast;
-    double forecast_2;
+    double pred_demand_1;
+    double pred_demand_2;
     double prev_forecast;
     double parameter_1 = 0.1;
     double parameter_2 = 0.2;
+   
     
     while (mnthcount <= 12)
     {
        cout << "Enter actual demand for month " << mnthcount++ << ": ";
        cin >> actual_demand;
        cout << endl;
-       
-        cout << "This is your prediction with 0.1: " << forecast << endl;
-            forecast = parameter_1 * actual_demand + ( 1 - parameter_1 ) *  actual_demand;
-            		cout.setf(ios::fixed);
-		        cout.setf(ios::showpoint);
-		        cout.precision(2);
-        cout << "This your prediction with 0.2: " << forecast << endl;
-            prev_forecast = forecast;
-            forecast_2 = parameter_2 * actual_demand + ( 1 - parameter_2 ) * prev_forecast;
-            		cout.setf(ios::fixed);
-		        cout.setf(ios::showpoint);
-		        cout.precision(2);
-		      cout << endl;
-    }
-    if (mnthcount == 1)
-    {
-      forecast = actual_demand; 
-      21forecast_2 = actual_demand;
-    }
-    else 
-    {
-      prev_act_demand = actual_demand;
-      prev_forecast = forecast;
-      forecast = parameter_1 * prev_act_demand + ( 1 - parameter_1 ) * prev_forecast;
+            
+             if (mnthcount == 1)
+                {
+                    cout << "This is your prediction: " << pred_demand_1 << endl;
+                        pred_demand_1 = parameter_1 * actual_demand + ( 1 - parameter_1 ) * actual_demand;
+                    cout << "This is your prediction: " << pred_demand_2 << endl;
+                        pred_demand_2 = parameter_2 * actual_demand + ( 1 - parameter_2 ) * actual_demand;
+                    cout << endl;
+                }
+             else 
+                {
+                    cout << "This is your prediction with 0.1: " << total_demand << endl;
+                   prev_act_demand = actual_demand;
+                   prev_forecast = pred_demand_1;
+                    total_demand = parameter_1 * prev_act_demand + ( 1 - parameter_1 ) * prev_forecast; 
+                        cout.setf(ios::fixed);
+		                cout.setf(ios::showpoint);
+		                cout.precision(2); 
+		                
+		            cout << "This your prediction with 0.2: " << total_demand_2 << endl;      
+                    prev_act_demand = actual_demand;
+                    prev_forecast = pred_demand_2;
+                    total_demand_2 = parameter_2 * prev_act_demand + ( 1 - parameter_2 ) * prev_forecast; 
+                        cout.setf(ios::fixed);
+		                cout.setf(ios::showpoint);
+		                cout.precision(2);
+            
+		             cout << endl;     
+		            
+                 }
       
-      prev_act_demand = actual_demand;
-      prev_forecast = forecast_2;
-      forecast_2 = parameter_2 * prev_act_demand + ( 1 - parameter_2 ) * prev_forecast;
     }
+   
     cout << endl;
     cout << "That is all I can Predict! " << endl;
     return 0;
